f9dcb6d5ce8d86310808a9220b2f10d4c3382f62
Viktor Asp <viktorasp23@gmail.com>
2025-03-02 20:17:57 +0100

    backup

--- src/main/java/venomized/mc/mods/swsignals/data/BlockStateDataGenerator.java
+++ src/main/java/venomized/mc/mods/swsignals/data/BlockStateDataGenerator.java
@@ -4,16 +4,15 @@ import net.minecraft.data.PackOutput;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import venomized.mc.mods.swsignals.SwSignal;
+import venomized.mc.mods.swsignals.block.SwBlocks;
 
 public class BlockStateDataGenerator extends BlockStateProvider {
 	public BlockStateDataGenerator(PackOutput output, ExistingFileHelper exFileHelper) {
 		super(output, SwSignal.MOD_ID, exFileHelper);
 	}
 
-	/**
-	 *
-	 */
 	@Override
 	protected void registerStatesAndModels() {
+		this.simpleBlock(SwBlocks.BLOCK_SW_SIGNAL_BOX.get(), cubeAll(SwBlocks.BLOCK_SW_SIGNAL_BOX.get()));
 	}
 }
