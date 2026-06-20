55b6424d8d6921c5e6e247f89aa41d4c8552cb33
VenomiXeD <crafterious@gmail.com>
2026-06-20 15:59:05 +0200

    (Germany) Update some translations for German blocks

--- germany/src/main/java/venomized/mods/extendedsignals/de/block/GermanyBlocks.java
+++ germany/src/main/java/venomized/mods/extendedsignals/de/block/GermanyBlocks.java
@@ -12,22 +12,27 @@ public class GermanyBlocks {
     // HP + VR
     public static final BlockEntry<BlockHVCombinedSignal> COMBINED_SIGNAL = RegistrateHelper
             .genericCustomModelledBlock(registrate(), "de", "signals", "hv_hp_vr", BlockHVCombinedSignal::new)
+            .lang("[HV] Combined Block and Distant Signal")
             .register();
     public static final BlockEntry<BlockHVMainBlockSignal> BLOCK_SIGNAL = RegistrateHelper
             .genericCustomModelledBlock(registrate(), "de", "signals", "hv_bk_hp_s", BlockHVMainBlockSignal::new)
+            .lang("[HV] Block Signal")
             .register();
     // HP, Hauptsignal
     public static BlockEntry<BlockHVMainSignal> MAIN_SIGNAL = RegistrateHelper
             .genericCustomModelledBlock(registrate(), "de", "signals", "hv_hp", BlockHVMainSignal::new)
+            .lang("[HV] Main Signal")
             .register();
 
     // VR, Vorsignal
     public static final BlockEntry<? extends Block> DISTANT_SIGNAL = RegistrateHelper
             .genericCustomModelledBlock(registrate(), "de", "signals", "hv_vr", BlockHVDistantSignal::new)
+            .lang("[HV] Distant Signal")
             .register();
 
     public static final BlockEntry<BlockZsCombinedSignal> ZS3_SIGNAL = RegistrateHelper
             .genericCustomModelledBlock(registrate(), "de", "signals", "hv_hp_vr_zs3", BlockZsCombinedSignal::new)
+            .lang("[HV] Combined block and Distant Signal with Zs3 and Zs3v indicator")
             .register();
 
     public static Registrate registrate() {
