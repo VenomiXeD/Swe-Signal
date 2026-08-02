4ab18701e0b4ef23618f6fc70d8094a08e7268f5
VenomiXeD <crafterious@gmail.com>
2026-08-02 20:15:34 +0200

    Revert ExtendedSignalsSweden.java to NeoForge 1.21.1, accidentally ported it from 1.20.1

--- sweden/src/main/java/venomized/mods/extendedsignals/se/ExtendedSignalsSweden.java
+++ sweden/src/main/java/venomized/mods/extendedsignals/se/ExtendedSignalsSweden.java
@@ -5,11 +5,12 @@ import com.tterrag.registrate.util.entry.RegistryEntry;
 import com.tterrag.registrate.util.nullness.NonNullSupplier;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraftforge.data.event.GatherDataEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import venomized.mods.extendedsignals.core.Mod;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
 import venomized.mods.extendedsignals.core.data.SoundEventDataGenerator;
 import venomized.mods.extendedsignals.se.block.SwedenBlocks;
 import venomized.mods.extendedsignals.se.blockentity.SwedenBlockEntities;
@@ -17,17 +18,20 @@ import venomized.mods.extendedsignals.se.client.SwedenModels;
 import venomized.mods.extendedsignals.se.data.SwedenRecipes;
 import venomized.mods.extendedsignals.se.item.SwedenItems;
 
-@net.minecraftforge.fml.common.Mod(ExtendedSignalsSweden.MOD_ID)
-public class ExtendedSignalsSweden extends Mod {
+@Mod(ExtendedSignalsSweden.MOD_ID)
+public class ExtendedSignalsSweden {
     public static final String MOD_ID = "extended_signals_se";
     public static final NonNullSupplier<Registrate> REGISTRATE = NonNullSupplier.lazy(() -> Registrate.create(MOD_ID));
 
-    public static final RegistryEntry<CreativeModeTab> CREATIVE_TAB = REGISTRATE.get().defaultCreativeTab(MOD_ID).register();
+    public static final RegistryEntry<CreativeModeTab, ?> CREATIVE_TAB = REGISTRATE.get().defaultCreativeTab(MOD_ID).register();
 
-    public ExtendedSignalsSweden(FMLJavaModLoadingContext context) {
-        super(context);
-        IEventBus bus = context.getModEventBus();
-        bus.register(this);
+    public ExtendedSignalsSweden(ModContainer mod) {
+        IEventBus bus = mod.getEventBus();
+        bus.register(ExtendedSignalsSweden.class);
+
+        SwedenBlocks.init();
+        SwedenItems.init();
+        SwedenBlockEntities.init();
 
         ExtendedSignalsSwedenSounds.init(bus);
     }
@@ -36,45 +40,8 @@ public class ExtendedSignalsSweden extends Mod {
         return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
     }
 
-
-    /**
-     *
-     */
-    @Override
-    protected void commonInitialization() {
-        SwedenBlocks.init();
-        SwedenItems.init();
-        SwedenBlockEntities.init();
-
-
-    }
-
-    /**
-     *
-     */
-    @Override
-    protected void clientInitialization() {
-        SwedenModels.init();
-    }
-
-    /**
-     * @return
-     */
-    @Override
-    protected RegistryEntry<CreativeModeTab> TAB_ENTRY() {
-        return CREATIVE_TAB;
-    }
-
-    /**
-     * @return
-     */
-    @Override
-    protected Registrate REGISTRATE() {
-        return REGISTRATE.get();
-    }
-
     @SubscribeEvent
-    public void onDataGeneration(GatherDataEvent e) {
+    public static void onDataGeneration(GatherDataEvent e) {
         e.getGenerator().addProvider(true, new SoundEventDataGenerator(
                 ExtendedSignalsSweden.MOD_ID,
                 e.getGenerator().getPackOutput(),
@@ -82,6 +49,9 @@ public class ExtendedSignalsSweden extends Mod {
                 ExtendedSignalsSwedenSounds.SOUNDS
         ));
 
-        e.getGenerator().addProvider(e.includeServer(), new SwedenRecipes(e.getGenerator().getPackOutput(), e.getLookupProvider()));
+        e.getGenerator().addProvider(
+                e.includeServer(),
+                new SwedenRecipes(e.getGenerator().getPackOutput(), e.getLookupProvider())
+        );
     }
 }
